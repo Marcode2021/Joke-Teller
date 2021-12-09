d37070373bb36c9327dc4a1b9393b204e57256e3
Marcode2021 <marcocode2021@gmail.com>
2021-12-09 08:20:21 +0100

    Add new Joke API, and add toggle disablity of btn

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const btn = document.querySelector("#button");
 const audio = document.querySelector("#audio");
+const key = "72298efc933846d4a929ea2b48349aea";
 // Get joke functionality, get joke from api and get content from it
 const getJoke = async function () {
-  const res = await fetch("https://dad-jokes.p.rapidapi.com/random/joke", {
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
-      "x-rapidapi-key": "3bf41993d0msh7778086c2033e29p11df6bjsn0503b17ebba1",
-    },
-  });
-  const data = await res.json();
-  return `${data.body[0].setup} ${data.body[0].punchline}`;
+  try {
+    const res = await fetch("https://v2.jokeapi.dev/joke/Any?type=single");
+    const data = await res.json();
+    return `${data.joke}`;
+  } catch (err) {
+    console.log(err);
+  }
 };
 //Add audio src to audio
-const displayJoke = async function () {
-  const joke = await getJoke();
-  const speach = `http://api.voicerss.org/?key=72298efc933846d4a929ea2b48349aea&hl=en-us&src=${joke}`;
-  audio.src = speach;
+const passJoke = async function () {
+  try {
+    const joke = await getJoke();
+    const speach = `http://api.voicerss.org/?key=${key}&hl=en-us&src=${joke}`;
+    audio.src = speach;
+  } catch (err) {
+    console.log(err);
+  }
 };
 //Add joke to audio
-displayJoke();
-
+passJoke();
+// Event listener
 btn.addEventListener("click", () => {
+  btn.disabled = true;
   audio.play();
+});
+audio.addEventListener("ended", () => {
+  btn.disabled = false;
 });
